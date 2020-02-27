ed8b47dde868a4f39cbbb3c22217241b598eb8bd
adc129 <60369328+adc129@users.noreply.github.com>
2020-02-26 23:24:24 -0600

    comment made in HelloWorld

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,17 +2,16 @@ package hello;
 /* Get a time library */
 import org.joda.time.LocalTime;
 
-// Add a comment
+// this is my comment hello 
 
-public class HelloWorld 
+public class HelloWorld
 {
 	/* master comment */
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
-    
+
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
 }
-
