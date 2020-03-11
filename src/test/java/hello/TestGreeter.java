b5fbe4bb95c9071dfc139075664ae044ec978ce0
Alejandra-G <60267587+Alejandra-G@users.noreply.github.com>
2020-03-11 18:06:14 -0500

    Update TestGreeter.java

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -32,4 +32,15 @@ public class TestGreeter {
       assertEquals(g.sayHello(),"Hello World!");
    }
 
+   @Test
+   @DisplayName("Test for Name='Alejandra Gonzalez'")
+   public void testGreeter()
+   {
+
+      g.setName("Alejandra Gonzalez");
+      assertEquals(g.getName(),"Alejandra Gonzalez");
+      assertEquals(g.sayHello(),"Alejandra Gonzalez");
+   }
+
+
 }
